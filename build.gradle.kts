07d8873dfb55f9c4fe2fd5cb214a4de6301cd8d2
smile_slime_47 <smile_slime_47@outlook.com>
2026-02-18 21:28:37 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,16 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
 }
 
 tasks.test {
